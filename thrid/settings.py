2329dec8a84e7869ea86fdaff3faae8e5437ef1c
ankitsh11 <ankitsh@live.in>
2020-11-01 08:19:23 +0000

    remove old stuff

--- thrid/settings.py
+++ thrid/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'wep)hr$d&=mrqh@*&^4r3o4j+095p99tkzpm!zk%jds^u64@!('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ankitsh11.pythonanywhere.com']
 
 
 # Application definition
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL='/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
